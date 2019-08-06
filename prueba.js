a075e25f6d14e77d274524918d0b84f339e0245f
hotelmodelo <52080802+hotelmodelo@users.noreply.github.com>
2019-08-06 15:50:24 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const http = require('http');
+
+const server = http.createServer(function (req, res){
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('Hola mundo');
+});
+
+console.log("Hola mundo");
+
+server.listen(3000);
